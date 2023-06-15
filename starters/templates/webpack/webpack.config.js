90bc242e2b41d70f818c5a8af499155820bc5e9c
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-06-15 14:25:02 +0100

    moved webpack to template

---
+++ starters/templates/webpack/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
