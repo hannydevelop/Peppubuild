29371a97671f245226aa584ee9dd7e024ca54492
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-08-01 21:34:56 +0100

    newrequest wip

---
+++ client/src/Newreq.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="">
+        <form @submit.prevent="sendFile">
+            <div class="form-group col-md-12">
+                <label for="exampleInputEmail">API name</label>
+                <input type="Text" class="form-control" name="exampleInputEmail" id="exampleInputEmail"
+                    placeholder="Describe Feature of design" v-model="title" />
+            </div>
+            <div class="form-group col-md-12">
+                <label for="exampleInputEmail">Description</label>
+                <input type="Text" class="form-control" name="exampleInputEmail" id="exampleInputEmail"
+                    placeholder="Describe Feature of design" v-model="title" />
+            </div>
+            <div class="form-group col-md-12">
+                <label for="exampleInputEmail">Params</label>
+            </div>
+            <div class="form-group col-md-12">
+                <label for="exampleInputEmail">Response</label>
+            </div>
+            <div class="form-check-inline col-md-1">
+                <select class="form-select" aria-label="Default select example">
+                    <option value="GET">GET</option>
+                    <option value="POST">One</option>
+                    <option value="PUT">Two</option>
+                    <option value="DELETE">Three</option>
+                </select>
+            </div>
+            <div class="form-check-inline col-md-6">
+                <input type="Text" class="form-control" name="exampleInputEmail" id="exampleInputEmail"
+                    placeholder="API Path" v-model="title" />
+            </div>
+            <div class="form-check-inline col-md-4">
+                <button type="button" class="btn btn-primary">Create</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default {
+    name: 'Newreq',
+}
+</script>
--- client/src/app.js
+++ client/src/app.js
@@ -2,11 +2,13 @@ import { createApp } from "vue";
 import Playground from './Playground.vue'
 import Home from './Home.vue'
 import App from './App.vue';
+import Newreq from './Newreq.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/playground', component: Playground }
+  { path: '/playground', component: Playground },
+  { path: '/newrequest', component: Newreq }
 ]
 
 const router = createRouter({
