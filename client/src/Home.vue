bfc99514d564e6674c76a4e1212e3cf556862dd4
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-09-21 21:36:20 +0100

    web page coming together

--- client/src/Home.vue
+++ client/src/Home.vue
@@ -580,29 +580,30 @@ export default {
       };
 
       editor.DomComponents.addType("custom-button", {
-      extend: "button",
-      view: {
-        events: {
-          dblclick: "handleDblClick"
-        },
-        handleDblClick() {
-          new bootstrap.Modal('#connectModal').show();
+        extend: "button",
+        view: {
+          events: {
+            dblclick: "handleDblClick"
+          },
+          handleDblClick() {
+            new bootstrap.Modal('#connectModal').show();
+          }
         }
-      }
-    });
+      });
 
-    editor.Blocks.add("Button", {
-      label: "Custom Button",
-      attributes: { class: "fa fa-square" },
-      content: {
-        type: "custom-button"
-      },
-      category: 'Extra'
-    });
+      editor.Blocks.add("Button", {
+        label: "Custom Button",
+        attributes: { class: "fa fa-square" },
+        content: {
+          type: "custom-button"
+        },
+        category: 'Extra'
+      });
     }
 
     this.editor = grapesjs.init({
       showOffsets: 1,
+      // dragMode: 'absolute',
       noticeOnUnload: 0,
       container: "#editor",
       storageManager: false,
@@ -666,7 +667,159 @@ export default {
             max: 1,
             min: 0,
           }]
-        }],
+        },
+        {
+          name: 'Flex',
+          open: false,
+          properties: [{
+            name: 'Flex Container',
+            property: 'display',
+            type: 'select',
+            defaults: 'block',
+            list: [
+              { value: 'block', name: 'Disable' },
+              { value: 'flex', name: 'Enable' }
+            ],
+          }, {
+            name: 'Flex Parent',
+            property: 'label-parent-flex',
+            type: 'integer',
+          }, {
+            name: 'Direction',
+            property: 'flex-direction',
+            type: 'radio',
+            defaults: 'row',
+            list: [{
+              value: 'row',
+              name: 'Row',
+              className: 'icons-flex icon-dir-row',
+              title: 'Row',
+            }, {
+              value: 'row-reverse',
+              name: 'Row reverse',
+              className: 'icons-flex icon-dir-row-rev',
+              title: 'Row reverse',
+            }, {
+              value: 'column',
+              name: 'Column',
+              title: 'Column',
+              className: 'icons-flex icon-dir-col',
+            }, {
+              value: 'column-reverse',
+              name: 'Column reverse',
+              title: 'Column reverse',
+              className: 'icons-flex icon-dir-col-rev',
+            }],
+          }, {
+            name: 'Justify',
+            property: 'justify-content',
+            type: 'radio',
+            defaults: 'flex-start',
+            list: [{
+              value: 'flex-start',
+              className: 'icons-flex icon-just-start',
+              title: 'Start',
+            }, {
+              value: 'flex-end',
+              title: 'End',
+              className: 'icons-flex icon-just-end',
+            }, {
+              value: 'space-between',
+              title: 'Space between',
+              className: 'icons-flex icon-just-sp-bet',
+            }, {
+              value: 'space-around',
+              title: 'Space around',
+              className: 'icons-flex icon-just-sp-ar',
+            }, {
+              value: 'center',
+              title: 'Center',
+              className: 'icons-flex icon-just-sp-cent',
+            }],
+          }, {
+            name: 'Align',
+            property: 'align-items',
+            type: 'radio',
+            defaults: 'center',
+            list: [{
+              value: 'flex-start',
+              title: 'Start',
+              className: 'icons-flex icon-al-start',
+            }, {
+              value: 'flex-end',
+              title: 'End',
+              className: 'icons-flex icon-al-end',
+            }, {
+              value: 'stretch',
+              title: 'Stretch',
+              className: 'icons-flex icon-al-str',
+            }, {
+              value: 'center',
+              title: 'Center',
+              className: 'icons-flex icon-al-center',
+            }],
+          }, {
+            name: 'Flex Children',
+            property: 'label-parent-flex',
+            type: 'integer',
+          }, {
+            name: 'Order',
+            property: 'order',
+            type: 'integer',
+            defaults: 0,
+            min: 0
+          }, {
+            name: 'Flex',
+            property: 'flex',
+            type: 'composite',
+            properties: [{
+              name: 'Grow',
+              property: 'flex-grow',
+              type: 'integer',
+              defaults: 0,
+              min: 0
+            }, {
+              name: 'Shrink',
+              property: 'flex-shrink',
+              type: 'integer',
+              defaults: 0,
+              min: 0
+            }, {
+              name: 'Basis',
+              property: 'flex-basis',
+              type: 'integer',
+              units: ['px', '%', ''],
+              unit: '',
+              defaults: 'auto',
+            }],
+          }, {
+            name: 'Align',
+            property: 'align-self',
+            type: 'radio',
+            defaults: 'auto',
+            list: [{
+              value: 'auto',
+              name: 'Auto',
+            }, {
+              value: 'flex-start',
+              title: 'Start',
+              className: 'icons-flex icon-al-start',
+            }, {
+              value: 'flex-end',
+              title: 'End',
+              className: 'icons-flex icon-al-end',
+            }, {
+              value: 'stretch',
+              title: 'Stretch',
+              className: 'icons-flex icon-al-str',
+            }, {
+              value: 'center',
+              title: 'Center',
+              className: 'icons-flex icon-al-center',
+            }],
+          }]
+        }
+        ],
       },
       traitManager: {
         appendTo: '#traits-container',
@@ -676,11 +829,13 @@ export default {
       },
       plugins: ["gjs-blocks-basic", gjsForms, myPlugin, grapeNav, plugin],
       pluginsOpts: {
-        "gjs-blocks-basic": {},
+        'gjs-blocks-basic': { flexGrid: true },
         gjsForms: {},
         grapeNav: {}
       },
     });
+
+    // this.editor.getModel().set('dmode', 'translate');
 
     this.editor.Panels.getPanels().reset([
       {
@@ -881,7 +1036,7 @@ export default {
       */
     }
 
-    
+
     let bm = this.editor.BlockManager;
 
     // Add a block
