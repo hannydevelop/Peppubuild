e2e47c1503fe33bf7ecdab8bec442277de0d2cd6
hannydevelop <ukpaiugochiibem@gmail.com>
2023-10-12 06:56:00 -0700

    made changes to peppu logo

--- packages/core/panel/src/index.js
+++ packages/core/panel/src/index.js
@@ -51,17 +51,6 @@ export default (editor, opts = {}) => {
     }
   )
 
-  // Add logo to panel
-  editor.Panels.addButton('options',
-    {
-      id: 'logo',
-      label: `<img src='https://www.linkpicture.com/q/IMG_2437-1.png' style="width:60%;height:50%;"/>`,
-      active: false,
-      command: 'peppu:logo'
-    }
-  )
-
-
   // option value for CSS values
   let cssString = `
     .gjs-one-bg {
@@ -82,6 +71,39 @@ export default (editor, opts = {}) => {
     .gjs-four-color-h:hover {
       color: ${options.bgColor.four};
     }
+
+    /* LOGO VERSION */
+
+    .gjs-pn-commands .gjs-pn-buttons,
+    #gjs-pn-commands .gjs-pn-buttons {
+      display: none;
+    }
+
+    .gjs-logo {
+      height: 25px;
+    }
+
+    .gjs-logo-cont {
+      position: relative;
+      display: inline-block;
+      top: 3px;
+    }
+
+    a:link {
+      text-decoration: none;
+    }
+
+    a:visited {
+      text-decoration: none;
+    }
+
+    a:hover {
+      text-decoration: none;
+    }
+
+    a:active {
+      text-decoration: none;
+    }
     `;
 
   // Append css styles to html
@@ -89,6 +111,52 @@ export default (editor, opts = {}) => {
   style.innerText = cssString;
   document.head.appendChild(style);
 
-  // peppu logo
-  editor.Commands.add('peppu:logo', editor => { window.open(options.url) });
+  var s = document.createElement('script');
+  s.innerText = function commandMod() {
+    editor.Modal.setTitle('About Peppubuild');
+    editor.Modal.setContent(
+      `<div>
+    <br/>
+    <div style="text-align: center">
+    <img src='https://www.linkpicture.com/q/IMG_2437-1.png' style="width:10%;height:5%;"/>
+    </div>
+    <br/>
+    <br/><br/>
+    <div>
+      <a class="gjs-four-color" target="_blank" href="peppubuild.com"><b>Peppubuild</b><a> is an attempt to build the ultimate
+      <a class="info-panel-link gjs-four-color" target="_blank" href="https://github.com/Peppu-Group/peppubuild">opensource fullstack nocode/lowcode tool</a>
+      for web applications.
+      <br/><br/>
+      For any hint about this project, check the
+      <ul>
+      <li>
+      <a class="info-panel-link gjs-four-color" target="_blank" href="https://docs.peppubuild.com">Peppubuild documentation</a>
+      </li>
+      <li>
+      <a class="info-panel-link gjs-four-color" target="_blank" href="https://github.com/artf/grapesjs-preset-webpage">
+      Read the contribution guideline</a>
+      and open an issue or start a discussion to contribute.
+      </li>
+      <li>
+      Checkout our <a class="info-panel-link gjs-four-color" target="_blank" href="https://github.com/artf/grapesjs-preset-webpage">milestone board</a> to see what we're up to.
+      </li>
+      </ul>
+      Being a free and open source project contributors and supporters are extremely welcome.
+    </div>
+  </div>`
+    );
+    editor.Modal.open();
+  }
+  document.body.appendChild(s)
+
+  editor.on('load', function () {
+    var logoCont = document.createElement('div');
+    logoCont.innerHTML = `
+    <div class="gjs-logo-cont">
+      <button onclick="commandMod()"><img class="gjs-logo" src="https://www.linkpicture.com/q/IMG_2437-1.png"></button>
+    </div>
+    `
+    var logoPanel = document.querySelector('.gjs-pn-commands');
+    logoPanel.appendChild(logoCont);
+  })
 };
