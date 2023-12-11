d2c0f228b05d9f5541a0d7b7c678313849a62f2d
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-12-11 03:42:43 +0100

    bundling peppubuild for release

--- server/index.js
+++ server/index.js
@@ -1,32 +1,14 @@
-// This file isn't processed by Vite, see https://github.com/brillout/vite-plugin-ssr/issues/562
-// Consequently:
-//  - When changing this file, you needed to manually restart your server for your changes to take effect.
-//  - To use your environment variables defined in your .env files, you need to install dotenv, see https://vite-plugin-ssr.com/env
-//  - To use your path aliases defined in your vite.config.js, you need to tell Node.js about them, see https://vite-plugin-ssr.com/path-aliases
-
-// If you want Vite to process your server code then use one of these:
-//  - vavite (https://github.com/cyco130/vavite)
-//     - See vavite + vite-pugin-ssr examples at https://github.com/cyco130/vavite/tree/main/examples
-//  - vite-node (https://github.com/antfu/vite-node)
-//  - HatTip (https://github.com/hattipjs/hattip)
-//    - You can use Bati (https://batijs.github.io/) to scaffold a vite-plugin-ssr + HatTip app. Note that Bati generates apps that use the V1 design (https://vite-plugin-ssr.com/migration/v1-design) and Vike packages (https://vite-plugin-ssr.com/vike-packages)
-
 import express from 'express'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
-import { renderPage } from 'vite-plugin-ssr/server'
-import { root } from './root.js'
 import * as fs from 'fs';
 import low from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync.js';
 import createProject from './utils/project.js'
-import runUpgrade from './utils/upgrade.js'
-import * as cron from 'node-cron'
 import * as path from 'path';
 import grapesjs from 'grapesjs';
 import replaceInFile from 'replace-in-file';
-import open from 'open';
 
 const isProduction = process.env.NODE_ENV === 'production'
 const adapter = new FileSync('db.json');
@@ -46,32 +28,6 @@ async function startServer() {
 
   app.use(compression())
   app.use(cookieParser())
-
-  // Vite integration
-  if (isProduction) {
-    // In production, we need to serve our static assets ourselves.
-    // (In dev, Vite's middleware serves our static assets.)
-    const sirv = (await import('sirv')).default
-    app.use(sirv(`${root}/dist/client`))
-  } else {
-    // We instantiate Vite's development server and integrate its middleware to our server.
-    // ⚠️ We instantiate it only in development. (It isn't needed in production and it
-    // would unnecessarily bloat our production server.)
-    const vite = await import('vite')
-    const viteDevMiddleware = (
-      await vite.createServer({
-        root,
-        server: { middlewareMode: true }
-      })
-    ).middlewares
-    app.use(viteDevMiddleware)
-  }
-
-  // call upgrade here
-  cron.schedule('* * * * *', () => {
-    runUpgrade();
-    console.log('running a task every minute');
-  });
 
   // set route for logout
   app.get('/logout', (_req, res) => {
@@ -447,32 +403,7 @@ async function startServer() {
     });
   })
 
-  // ...
-  // Other middlewares (e.g. some RPC middleware such as Telefunc)
-  // ...
-
-  // Vite-plugin-ssr middleware. It should always be our last middleware (because it's a
-  // catch-all middleware superseding any middleware placed after it).
-  app.get('*', async (req, res, next) => {
-    const pageContextInit = {
-      urlOriginal: req.originalUrl,
-      user: req.user
-    }
-    const pageContext = await renderPage(pageContextInit)
-    const { httpResponse } = pageContext
-    if (!httpResponse) {
-      return next()
-    } else {
-      const { body, statusCode, headers, earlyHints } = httpResponse
-      if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
-      headers.forEach(([name, value]) => res.setHeader(name, value))
-      res.status(statusCode)
-      // For HTTP streams use httpResponse.pipe() instead, see https://vite-plugin-ssr.com/stream
-      res.send(body)
-    }
-  })
-
   const port = process.env.PORT || 1404
   app.listen(port)
-  await open(`http://localhost:${port}`);
+  console.log(`started server in ${port}`)
 }
