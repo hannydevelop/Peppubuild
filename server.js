e5c66ef84ad892325cef2beb254ac691db33cc16
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2023-12-14 11:17:10 +0100

    replaced index with server

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 "use strict";
-exports.__esModule = true;
-exports.startServer = void 0;
-
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
