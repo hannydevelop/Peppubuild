44c692fe57f2adf12e97dc0fac2250f018d906d6
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2024-01-18 13:33:44 +0100

    slugs dont work

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -6,7 +6,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
