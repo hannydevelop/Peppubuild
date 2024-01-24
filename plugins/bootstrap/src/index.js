2d9a6443b7491637868e1950b740061b85b711d4
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2024-01-24 10:54:24 +0100

    fixed accordion

--- plugins/bootstrap/src/index.js
+++ plugins/bootstrap/src/index.js
@@ -17,13 +17,4 @@ export default (editor, opts = {}) => {
       en,
       ...options.i18n,
   });
-
-  // TODO Remove
-  editor.on('load', () =>
-    editor.addComponents(
-        `<div style="margin:100px; padding:25px;">
-            Content loaded from the plugin
-        </div>`,
-        { at: 0 }
-    ))
 };
