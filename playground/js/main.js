fff7dc2b0b58c44e750cf709f5943aac4a82e692
hannydevelop <ukpaiugochiibem@gmail.com>
2024-03-15 13:04:33 +0100

    added modal info for navbar

--- playground/js/main.js
+++ playground/js/main.js
@@ -8,6 +8,7 @@ import {
     GoogleAuthProvider,
     GithubAuthProvider
 } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+const mdl = editor.Modal;
 // Create route components
 const Home = {
     template: `
@@ -234,7 +235,52 @@ const Home = {
             }
         });
         editor.on('block:drag:stop', (component, block) => {
-            console.log(component)
+            if (component.attributes.name == 'B-NAVBAR') {
+                mdl.open({
+                    title: 'This is s Navigation Bar',
+                    content: `You just dropped a navigation bar. Navigation bars are usually a list of links, 
+                    useful to aid visitors to access information faster. This navigation bar is built with Bootstrap, to create
+                    yours, add the following to your HTML file:
+                    <nav class="navbar navbar-expand-lg fixed-top navbar-light">
+                    <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Navbar</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                    <button type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler">Send</button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                        </ul>
+                        <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                    </div>
+                </nav>
+                `,
+                })
+            }
         });
     }
 }
