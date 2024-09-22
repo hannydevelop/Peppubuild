fd8763cc12ad8cea20045f22ab28c9480d2ad244
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2024-09-22 04:14:41 +0100

    fixed dashboard vue

---
+++ playground/src/views/LogPage.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container-fluid">
+    <div class="row flex-nowrap">
+      <SideBar />
+      <div class="col py-3">
+        <div class="dash-content" id="d-cont">
+          <div id="i2sw">Projects Overview
+          </div>
+          <div class="row">
+            <div class="col-sm-3">
+              <div class="card text-success border-success">
+                <div class="card-header">Total visits</div>
+                <div class="card-body">
+                  <h1 class="card-title">1,000</h1>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card text-success border-success">
+                <div class="card-header">Current Plan</div>
+                <div class="card-body">
+                  <h1 class="card-title">Community</h1>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card text-success border-success">
+                <div class="card-header">Projects</div>
+                <div class="card-body">
+                  <h1 class="card-title">14</h1>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="card text-success border-success">
+                <div class="card-header">Collaborators</div>
+                <div class="card-body">
+                  <h1 class="card-title">1</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div id="i2sw">Total Visits
+          </div>
+          <Line id="my-chart-id" :options="chartOptions" :data="chartData" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import SideBar from '../components/SideBar.vue';
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+} from 'chart.js'
+
+ChartJS.register(Title, Filler, Tooltip, Legend, PointElement, LineElement, CategoryScale, LinearScale)
+
+
+export default {
+  name: 'LogPage',
+  components: { SideBar, Line },
+  data() {
+    return {
+      chartData: {
+        labels: ["1 Dec", "8 Dec", "16 Dec", "31 Dec"],
+    datasets: [
+      {
+        label: "Sales Made",
+        data: [3, 7, 4, 5],
+        borderColor: ["#90ee90"],
+        fill: true,
+        backgroundColor: '#F1F2F3'
+      },
+    ],
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+
+</script>
---
+++ playground/src/views/js/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+
+
+// TODO: Replace the following with your app's Firebase project configuration 
+// See: https://firebase.google.com/docs/web/learn-more#config-object 
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTHDOMAIN,
+    projectId: process.env.VUE_APP_PROJECTID,
+    storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_SENDERID,
+    appId: process.env.VUE_APP_APPID,
+    measurementId: process.env.VUE_APP_MEASUREMENTID
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const cpanelSub = {
+    cpanelDomain: process.env.VUE_APP_CPANEL_DOMAIN,
+    root: 'peppubuild.com',
+    cpanelApiKey: process.env.VUE_APP_CPANEL_SECRET_KEY,
+    cpanelUsername: process.env.VUE_APP_CPANEL_USER
+}
+// Initialize Firebase Authentication and get a reference to the service
+export const userAuth = getAuth();
+export const cpanelSubConfig = cpanelSub;
